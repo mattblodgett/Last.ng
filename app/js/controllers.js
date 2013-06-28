af97f27acee2c83877e9ef81eb453fa2bf90812b
Matt Blodgett <mattblodgett@gmail.com>
2013-06-27 21:42:27 -0400

    WIP 6-27-2013

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', function($scope) {
+  	$scope.greeting = "Hello, world";
   }])
   .controller('MyCtrl2', [function() {
 
