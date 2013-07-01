ef1bfd8cac11efbdab379a24f7e8509458e2a6b5
Matt Blodgett <mattblodgett@gmail.com>
2013-07-01 17:04:46 -0400

    WIP 7-1-2013 afternoon

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,12 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', ['$scope', function($scope) {
   	$scope.greeting = "Hello, world";
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl2', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+  	$scope.username = $routeParams.username;
+  	var lastFMUrl = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=" + $routeParams.username + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
+  	$http.jsonp(lastFMUrl).
+  	  success(function (data) {
+        console.log(data);
+        $scope.data = data;
+  	  });
   }]);
