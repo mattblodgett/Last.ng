9210019ab43aa9e6a17b10d41038f73983614d1b
Matt Blodgett <mattblodgett@gmail.com>
2013-07-01 21:33:33 -0400

    WIP 7-1-2013 night

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/user/:username', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/user/:username/artists', {templateUrl: 'partials/user-artists.html', controller: 'UserCtrl'});
+    $routeProvider.when('/artist/:artist/albums', {templateUrl: 'partials/artist-albums.html', controller: 'ArtistCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,15 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope', function($scope) {
+  controller('HomeCtrl', ['$scope', function($scope) {
   	$scope.greeting = "Hello, world";
   }])
-  .controller('MyCtrl2', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+  .controller('UserCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
   	$scope.username = $routeParams.username;
   	var lastFMUrl = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=" + $routeParams.username + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
   	$http.jsonp(lastFMUrl).
   	  success(function (data) {
-        console.log(data);
-        $scope.data = data;
+        $scope.data = data.topartists.artist;
+  	  });
+  }])
+  .controller('ArtistCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+  	$scope.artist = $routeParams.artist;
+  	var lastFMUrl = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + $routeParams.artist + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
+  	$http.jsonp(lastFMUrl).
+  	  success(function (data) {
+        $scope.data = data.topalbums.album;
   	  });
   }]);
