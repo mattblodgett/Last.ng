9f568ae895ef61a697b9734c85ae05ccdef8fea6
Matt Blodgett <mattblodgett@gmail.com>
2013-07-09 21:36:51 -0400

    Implemented a basic version of the Last.fm user homepage

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,7 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
-    $routeProvider.when('/user/:username/artists', {templateUrl: 'partials/user-artists.html', controller: 'UserCtrl'});
+    $routeProvider.when('/user/:username', {templateUrl: 'partials/user-home.html', controller: 'UserCtrl'});
     $routeProvider.when('/artist/:artist/albums', {templateUrl: 'partials/artist-albums.html', controller: 'ArtistCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,14 +6,45 @@ angular.module('myApp.controllers', []).
   controller('HomeCtrl', ['$scope', function($scope) {
   	$scope.greeting = "Hello, world";
   }])
+
   .controller('UserCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
   	$scope.username = $routeParams.username;
-  	var lastFMUrl = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=" + $routeParams.username + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
-  	$http.jsonp(lastFMUrl).
+
+  	var recentTracksUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + $routeParams.username + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
+  	$http.jsonp(recentTracksUrl).
   	  success(function (data) {
-        $scope.data = data.topartists.artist;
+        $scope.recentTracks = data.recenttracks.track.map(function (track) {
+          return {
+            artist: track.artist["#text"],
+            song: track.name
+          };
+        });
   	  });
+
+    var topArtistsUrl = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&limit=15&user=" + $routeParams.username + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
+    $http.jsonp(topArtistsUrl).
+      success(function (data) {
+        $scope.artists = data.topartists.artist.map(function (artist) {
+          return {
+            name: artist.name,
+            plays: artist.playcount
+          };
+        });
+      });
+
+    var topTracksUrl = "http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&limit=15&user=" + $routeParams.username + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
+    $http.jsonp(topTracksUrl).
+      success(function (data) {
+        $scope.topTracks = data.toptracks.track.map(function (track) {
+          return {
+            artist: track.artist.name,
+            song: track.name,
+            plays: track.playcount
+          };
+        });
+      });
   }])
+
   .controller('ArtistCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
   	$scope.artist = $routeParams.artist;
   	var lastFMUrl = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + $routeParams.artist + "&api_key=ee3dc61e84dfd72067a520d0cb55300b&format=json&callback=JSON_CALLBACK";
